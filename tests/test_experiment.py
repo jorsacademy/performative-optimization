6332402016a675ae62944a73288766f2ff0427f0
Jors Academy <abalci16@posta.pau.edu.tr>
2026-09-17 21:44:52 +0300

    Format experiment-test import block for Ruff

--- tests/test_experiment.py
+++ tests/test_experiment.py
@@ -1,4 +1,7 @@
-from performative_optimization.config import EnvironmentConfig, ExperimentConfig
+from performative_optimization.config import (
+    EnvironmentConfig,
+    ExperimentConfig,
+)
 from performative_optimization.environment import PerformativeDemandEnvironment
 from performative_optimization.experiment import run_benchmark, run_single_seed
 
